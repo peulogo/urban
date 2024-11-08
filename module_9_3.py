33678ad7ae3d2608610c6bd45832cdbdd576bbcb
peulogo <99072707+peulogo@users.noreply.github.com>
2024-11-09 01:13:18 +0300

    Create module_9_3.py

---
+++ module_9_3.py
@@ -0,0 +1,9 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = [len(word1) - len(word2) for word1, word2 in zip(first, second) if len(word1) != len(word2)]
+
+second_result = [len(first[i]) - len(second[i]) == 0 for i in range(min(len(first), len(second)))]
+
+print(list(first_result))
+print(list(second_result))
