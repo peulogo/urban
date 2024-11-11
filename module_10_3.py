a6f79bd3f86cd9a66e45a2bb638146f1c93bcd90
peulogo <99072707+peulogo@users.noreply.github.com>
2024-11-11 19:02:58 +0300

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -12,7 +12,8 @@ class Bank:
             amount = randint(50, 500)
             self.lock.acquire()
             self.balance += amount
-            print(f"Пополнение: {i}. Баланс: {self.balance}")
+            print(f"Пополнение: {amount}. Баланс: {self.balance}")
+            
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
             else:
@@ -23,19 +24,20 @@ class Bank:
     def take(self):
         for i in range(100):
             amount = randint(50, 500)
-            self.lock.acquire()
             print(f"Запрос на {amount}")
+            self.lock.acquire()
             if amount <= self.balance:
                 self.balance -= amount
                 print(f"Снятие: {amount}. Баланс: {self.balance}")
             else:
                 print('Запрос отклонён, недостаточно средств')
             self.lock.release()
-            
+            time.sleep(0.001)
+
 bk = Bank()  
 
-th1 = threading.Thread(target=Bank.deposit, args=(bk,))
-th2 = threading.Thread(target=Bank.take, args=(bk,))
+th1 = threading.Thread(target=bk.deposit)
+th2 = threading.Thread(target=bk.take)
 
 th1.start()
 th2.start()
@@ -43,4 +45,3 @@ th1.join()
 th2.join()
 
 print(f'Итоговый баланс: {bk.balance}')
-
