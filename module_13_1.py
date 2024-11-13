30d471c316d9525110bb8106e5e137ad68ab435f
peulogo <99072707+peulogo@users.noreply.github.com>
2024-11-13 21:28:07 +0300

    Create module_13_1.py

---
+++ module_13_1.py
@@ -0,0 +1,17 @@
+import asyncio
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования')
+    for i in range(1,6):
+        await asyncio.sleep(power)
+        print(f'Силач {name} поднял {i} шар')
+    print(f'Силач {name} закончил соревнования.')
+
+async def start_tournament():
+    await asyncio.gather(
+        start_strongman('Pasha', 3),
+        start_strongman('Denis', 4),
+        start_strongman('Apollon', 5)
+    )
+
+asyncio.run(start_tournament())
