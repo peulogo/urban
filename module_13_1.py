95284a6dfd80707ed14d1fe708fe74dfe24be331
peulogo <99072707+peulogo@users.noreply.github.com>
2024-11-13 21:29:09 +0300

    Update module_13_1.py

--- module_13_1.py
+++ module_13_1.py
@@ -3,7 +3,7 @@ import asyncio
 async def start_strongman(name, power):
     print(f'Силач {name} начал соревнования')
     for i in range(1,6):
-        await asyncio.sleep(power)
+        await asyncio.sleep(1 / power)
         print(f'Силач {name} поднял {i} шар')
     print(f'Силач {name} закончил соревнования.')
 
