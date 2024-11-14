ca8d9b7fd2031692e470f8ee63da5105027c60ec
peulogo <99072707+peulogo@users.noreply.github.com>
2024-11-14 10:14:53 +0300

    Create module_13_3.py

---
+++ module_13_3.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = '7777011691:AAE_Xwde827Hfec4pJq9lWW1YBJiM89WMpM'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_message(message: types.Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
