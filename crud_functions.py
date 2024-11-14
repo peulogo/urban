90fce61ea3e85b6c6d71082b5ccc19aab9f97ba6
peulogo <99072707+peulogo@users.noreply.github.com>
2024-11-14 17:20:18 +0300

    Update crud_functions.py

--- crud_functions.py
+++ crud_functions.py
@@ -13,9 +13,45 @@ def initiate_db(db_name='not_telegram.db'):
             price INTEGER NOT NULL
         )
     ''')
+    conn.commit()
+
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Users (
+                id INTEGER PRIMARY KEY,
+                username TEXT NOT NULL,
+                email TEXT NOT NULL,
+                age INTEGER NOT NULL,
+                balance INTEGER NOT NULL
+            )
+        ''')
+    conn.commit()
+    conn.close()
+
+
+def add_user(username, email, age, balance, db_name='not_telegram.db'):
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO Users (username, email, age, balance)
+        VALUES (?, ?, ?, ?)
+        ''', (username, email, age, balance))
 
     conn.commit()
     conn.close()
+
+
+def is_included(username, db_name='not_telegram.db'):
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    SELECT 1 FROM Users WHERE username = ?
+    ''', (username,))
+
+    result = cursor.fetchone()
+    conn.close()
+    return result is not None
+
 
 def get_all_products(db_name='not_telegram.db'):
     conn = sqlite3.connect(db_name)
